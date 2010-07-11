0176aac408869b1521e95f4850d532291d621201
Bo Ferri <zazi@smiy.org>
2010-07-11 16:19:18 +0000

    orderedlistonto: * MODIFIED: examples

---
+++ examples/gen_turtle.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+# Generate the Turtle from the RDF/XML code
+
+rapper -i rdfxml -o turtle RDF/orderedlist_-_example.rdf > N3/orderedlist_-_example.n3
---
+++ examples/gen_xml.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+# Generate the RDF/XML from the Turtle code
+
+rapper -i turtle -o rdfxml-abbrev N3/orderedlist_-_example.n3 > RDF/orderedlist_-_example.rdf
